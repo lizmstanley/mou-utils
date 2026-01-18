87c5962da36daae349a0b9ce0f7a27f20ee63f7e
Liz Stanley <lizmstanley@gmail.com>
2026-01-18 16:28:43 -0600

    Adjust checking python version

--- setup.sh
+++ setup.sh
@@ -4,10 +4,30 @@ if ! [[ "${BASH_SOURCE[0]}" != "$0" ]]; then
   exit 1
 fi
 
-if ! command -v pyenv >/dev/null 2>&1; then
+# Get required major.minor from .python-version
+required_version=$(head -n 1 .python-version | cut -d. -f1,2)
+# Get current python major.minor version
+current_version=$(python --version 2>&1 | awk '{print $2}' | cut -d. -f1,2)
+not_current_python=false
+if [ "$current_version" != "$required_version" ]; then
+  echo "Python version mismatch: required $required_version, found $current_version"
+  not_current_python=true
+fi
+
+if $not_current_python && ! command -v pyenv >/dev/null 2>&1; then
   echo "pyenv not found, installing..."
+  rm -rf "$HOME/.pyenv" devnull 2>&1
   curl -fsSL https://pyenv.run | bash
 fi
+
+if $not_current_python; then
+  echo "Installing required Python version via pyenv..."
+  export PATH="$HOME/.pyenv/bin:$PATH"
+  eval "$(pyenv init --path)"
+  # shellcheck disable=SC2002
+  cat .python-version | xargs pyenv install
+fi
+python -m venv ./.venv && source ./.venv/bin/activate
 
 command -v poetry > /dev/null 2>&1
 poetry_command_exists=$?
@@ -18,9 +38,6 @@ if [[ $poetry_command_exists -ne 0 || $poetry_installed_for_active_python -ne 0 
   echo "Poetry not found, installing..."
   pip install poetry
 fi
-# shellcheck disable=SC2002
-cat .python-version | xargs pyenv install
-python -m venv ./.venv && source ./.venv/bin/activate
 poetry lock && poetry install
 if ! [ -f .env ]; then
   echo ".env file not found, copying sample.env to .env"
